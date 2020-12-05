b4e1f66ca4c9b145f3ff46c019fe9133223af337
kashishvjain <66744312+kashishvjain@users.noreply.github.com>
2020-12-05 22:25:31 +0530

    Added templates

--- hackathon/imagesrec/views.py
+++ hackathon/imagesrec/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome Guys !")
+    return render(request,'home.html')
